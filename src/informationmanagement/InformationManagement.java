6766cb8303958a0af62042650a967026a7a61828
cfsloth <claudiofilipesilvagoncalves@gmail.com>
2017-09-13 03:52:17 +0100

    Need to find a new login image

--- src/informationmanagement/InformationManagement.java
+++ src/informationmanagement/InformationManagement.java
@@ -22,12 +22,12 @@ public class InformationManagement extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("FluxI! - Management of Information");
         stage.show();
     }
-
-    /**
+   /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
