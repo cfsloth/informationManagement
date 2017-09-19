5122beb45cc56c12fd7b9671e599520e765f5c7b
cfsloth <claudiofilipesilvagoncalves@gmail.com>
2017-09-19 14:48:31 +0100

    + Some Changes on paths + Simple stuff to do

--- src/com/informationmanagement/viewer/MenuUIController.java
+++ src/com/informationmanagement/viewer/MenuUIController.java
@@ -7,6 +7,7 @@ package com.informationmanagement.viewer;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +16,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 /**
@@ -32,20 +35,47 @@ public class MenuUIController implements Initializable {
         
     }
 
-    @FXML
-    private void printDone(ActionEvent envent) throws IOException{
-       
-        
+     @FXML
+    private void logOut(ActionEvent event) throws IOException{
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Sign Out");
+        alert.setHeaderText("Deseja mesmo sair do sistema?");
+        alert.setContentText("Se sair do sistema, dados que não tenham sido \nguardados serão perdidos.");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+            System.out.println("Changing to Login");
+            Node source = (Node) event.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            Parent menu = FXMLLoader.load(getClass().getResource("LoginUI.fxml"));
+            Scene scene = new Scene(menu);
+            stage.setScene(scene);
+        } 
+    }
+    
+     @FXML
+    private void goToUserPage(ActionEvent event) throws IOException{
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Go to User Page");
+        alert.setHeaderText("Deseja mesmo ir para o seu perfil?");
+        alert.setContentText("Se sair do sistema, dados que não tenham sido \nguardados serão perdidos.");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+            System.out.println("Changing to UserUI");
+            Node source = (Node) event.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            Parent menu = FXMLLoader.load(getClass().getResource("UserUI.fxml"));
+            Scene scene = new Scene(menu);
+            stage.setScene(scene);
+        } 
     }
     
     @FXML
-    private void warningUI(ActionEvent event) throws IOException{
-        //Changing scenes
+    private void goToWarningPage(ActionEvent event) throws IOException{
         System.out.println("Changing to WarningUI");
         Node source = (Node) event.getSource();
-        
         Stage stage = (Stage) source.getScene().getWindow();
-        
         Parent menu = FXMLLoader.load(getClass().getResource("WarningUI.fxml"));
         Scene scene = new Scene(menu);
         stage.setScene(scene);
